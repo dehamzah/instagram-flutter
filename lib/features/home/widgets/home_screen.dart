ab80be910dab29c43aa721fd082a03752c814f63
hamzah <hamzah@alamat.com>
2019-10-30 06:20:12 +0700

    merge from master

--- lib/features/home/widgets/home_screen.dart
+++ lib/features/home/widgets/home_screen.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/core/icons/instagram_icons.dart';
+import 'package:instagram_flutter/core/theme/dimens.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        leading: Text('Instagram'),
+        middle: Container(
+          width: double.infinity,
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.only(
+            top: Dimens.space8,
+            bottom: Dimens.space8,
+          ),
+          child: Image.asset(
+            'assets/images/instagram.png',
+          ),
+        ),
+        leading: Icon(InstagramIcons.photo_outline),
+        trailing: Icon(InstagramIcons.inbox_outlined),
       ),
       child: Container(
         child: Text('Home Screen'),
