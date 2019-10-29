96e1e7d7eebf654259c039e7702bb46c15995563
hamzah <hamzah@alamat.com>
2019-10-29 22:10:23 +0700

    add appbar

--- lib/features/home/widgets/home_screen.dart
+++ lib/features/home/widgets/home_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/core/theme/dimens.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        leading: Text('Instagram'),
+        middle: Container(
+          width: double.infinity,
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.only(
+            top: Dimens.space8,
+            bottom: Dimens.space8,
+          ),
+          child: Image.asset(
+            'assets/images/instagram.png',
+          ),
+        ),
+        trailing: Icon(CupertinoIcons.share),
+        leading: Icon(CupertinoIcons.photo_camera),
       ),
       child: Container(
         child: Text('Home Screen'),
